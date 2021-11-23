b511a152711da58069dbb4e75d9f4af85e6417f7
Andres Alfaro <billoalfaro@gmail.com>
2021-11-22 20:53:47 -0600

    Se agrega descuentos.js

---
+++ descuentos.js
@@ -0,0 +1,33 @@
+/* const precioOriginal = 120;
+const descuento = 18;
+
+const porcentajePrecioConDescuento = 100 - descuento;
+const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100; */
+
+/* console.log({
+    precioOriginal,
+    descuento,
+    porcentajePrecioConDescuento,
+    precioConDescuento,
+}); */
+
+
+function calcularPrecioCondescuento(precio, descuento) {
+    const porcentajePrecioConDescuento = 100 - descuento;
+    const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+    return precioConDescuento;
+}
+
+function onClickButtonPriceDiscount() {
+    const inputPrice = document.getElementById("InputPrice");
+    const priceValue = inputPrice.value;
+    const inputDiscount = document.getElementById("InputDiscount");
+    const discountValue = inputDiscount.value;
+
+    const precioConDescuento = calcularPrecioCondescuento(priceValue, discountValue);
+
+
+    const resultP = document.getElementById("ResultP");
+    resultP.innerText = "El Precio es de  $" + precioConDescuento;
+}
